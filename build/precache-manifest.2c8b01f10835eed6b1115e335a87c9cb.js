02e8d01d47f221093cb467ff258935cd999584f4
Yui Xiong Ng <jadebear15@gmail.com>
2022-10-02 21:57:51 +0800

    feat:deployed production

---
+++ build/precache-manifest.2c8b01f10835eed6b1115e335a87c9cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41e1f565c1aa7ad35d7f167d5678be42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60b482588daaded170cf",
+    "url": "/static/css/main.3be36971.chunk.css"
+  },
+  {
+    "revision": "796ec7f94b663093e482",
+    "url": "/static/js/2.e8c542a8.chunk.js"
+  },
+  {
+    "revision": "60b482588daaded170cf",
+    "url": "/static/js/main.e3d3fa21.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
